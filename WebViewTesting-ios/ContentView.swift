65c2e61468f44ea9feb9056141cbc3ed013e3f33
OrenZak <orenz@wix.com>
2020-09-13 15:41:42 +0300

    Initial Commit

---
+++ WebViewTesting-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebViewTesting-ios
+//
+//  Created by Oren Zakay on 13/09/2020.
+//  Copyright © 2020 Oren Zakay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebViewTesting-iosTests/WebViewTesting_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  WebViewTesting_iosTests.swift
+//  WebViewTesting-iosTests
+//
+//  Created by Oren Zakay on 13/09/2020.
+//  Copyright © 2020 Oren Zakay. All rights reserved.
+//
+
+import XCTest
+@testable import WebViewTesting_ios
+
+class WebViewTesting_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
